ea670053bc68beb3131d4c21da8f59182c87859b
Armin Hashemi <94608961+RandomUserUsingGitHub@users.noreply.github.com>
2023-05-28 12:28:21 +0330

    Add files via upload

--- graphics/matrices.py
+++ graphics/matrices.py
@@ -3,8 +3,6 @@ import math
 import numpy as np
 
 def original():
-    x.append(x[0])
-    y.append(y[0])
 
     plt.figure()
     plt.plot(x, y, marker='o')
@@ -17,11 +15,10 @@ def calculate(matrix):
                             [y[idx]],
                             [1]], dtype=float)
         dot = np.dot(matrix, current)
-        x[idx] = dot[0]
-        y[idx] = dot[1]
-
-    if(input("continue?(1/0) ----> ") == '1'):
-        get_user_input()
+        x[idx] = dot[0][0]
+        y[idx] = dot[1][0]
+    print(x)
+    print(y)
     
 
 def init_transform():
@@ -32,16 +29,24 @@ def init_transform():
                                    [0,0,1]], dtype=float)
     calculate(translation_matrix)
 
+def transform_for_rotation(x_transition,y_transition):
+    translation_matrix = np.array([[1,0,x_transition],
+                                   [0,1,y_transition],
+                                   [0,0,1]], dtype=float)
+    calculate(translation_matrix)
+    
 def init_rotation():
     tetha = float(input("enter rotaion in degrees: "))
     tetha = math.radians(tetha)
     x = int(input("around what x-axis: "))
     y = int(input("around what y-axis: "))
-    rotation_matrix = np.array([[math.cos(tetha),-math.sin(tetha),x],
-                                [math.sin(tetha), math.cos(tetha),y],
-                                [       0       ,        0       ,1]],
+    transform_for_rotation(x*-1,y*-1)
+    rotation_matrix = np.array([[math.cos(tetha),-1*math.sin(tetha),0],
+                                [math.sin(tetha),   math.cos(tetha),0],
+                                [       0       ,         0        ,1]],
                                 dtype=float)
     calculate(rotation_matrix)
+    transform_for_rotation(x,y)
 
 def init_resize():
     x_resize = input("enter resize value of x-axis: ")
@@ -53,7 +58,7 @@ def init_resize():
     calculate(resize_matrix)
 
 def get_user_input():
-    action = input("select action:\n1) transition\n2) rotation\n3) resize\n")
+    action = input("select action:\n1) transition\n2) rotation\n3) resize\nyour option: ")
     match action:
         case '1':
             init_transform()
@@ -62,12 +67,10 @@ def get_user_input():
         case '3':
             init_resize()
 
-# Get user input for the three points of the triangle
 x = []
 y = []
 sides = int(input(f"How many sides the shape should have? "))
 for idx in range(sides):
-    print(idx)
     x.append(int(input(f"Enter x-coordinate of point {idx+1}: ")))
     y.append(int(input(f"Enter y-coordinate of point {idx+1}: ")))
     print(f"i{idx}({x[idx]},{y[idx]})")
@@ -77,6 +80,10 @@ y.append(y[0])
 
 original()
 get_user_input()
+
+while(input("continue?(1/0) ----> ") == '1'):
+        get_user_input()
+
 plt.figure()
 plt.plot(x, y, marker='o')
 plt.title("after matrices")
